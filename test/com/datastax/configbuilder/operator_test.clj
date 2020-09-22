beff68604e5a93dfcccc4cc8e59ec1faa6eae1f7
John Trimble <trimblej@gmail.com>
2020-09-22 14:50:32 -0700

    Fix test to actually test something

--- test/com/datastax/configbuilder/operator_test.clj
+++ test/com/datastax/configbuilder/operator_test.clj
@@ -5,7 +5,8 @@
   (:require [com.datastax.configbuilder.operator :as sut]
             [com.datastax.configbuilder.test-helpers :as helper]
             [clojure.java.shell :refer [sh]]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.set :as set]))
 
 (use-fixtures
   :each
@@ -27,7 +28,7 @@
         (is (= "Unexpected end-of-input within/between Object entries\n at [Source: (StringReader); line: 1, column: 31]"
                (.getMessage e))
             "Wrong error thrown"))))
-  (testing "simple rendering test reading input from the environment"
+  (testing "ensure all files generated"
     (sut/make-configs
      {:pod-ip "127.0.0.1"
       :config-file-data "{\"cluster-info\":{\"name\":\"test-cluster\",\"seeds\":\"1.2.3.1,1.2.3.2\"},\"datacenter-info\":{\"name\":\"test-dc\",\"graph-enabled\":false,\"solr-enabled\":false,\"spark-enabled\":false},\"node-info\":{\"name\":\"test-node\",\"rack\":\"test-rack\",\"listen_address\":\"1.2.3.4\",\"native_transport_address\":\"1.2.3.6\",\"native_transport_broadcast_address\":\"1.2.3.7\",\"initial_token\":\"12345\",\"auto_bootstrap\":false,\"agent_version\":\"6.8.0SNAPSHOT\"},\"cassandra-yaml\":{\"disk_access_mode\":\"foo\"}}"
@@ -36,9 +37,6 @@
       :rack-name "default"
       :config-output-directory (str @helper/temp-dir)
       :definitions-location "cass-config-definitions/resources"})
-    (doseq [current-file (.list (.toFile @helper/temp-dir))]
-      (let [diff-result (sh "diff"
-                            (str @helper/temp-dir "/" current-file)
-                            (str "test/data/configs/dse-6.0.11/" current-file))]
-        (is (= 0 (:exit diff-result))
-            (str "Rendered output for " current-file " does not match expected output.\nDiff:\n" (:out diff-result)))))))
+    (is (= #{} (set/difference
+      #{"logback.xml" "cassandra-rackdc.properties" "cassandra-env.sh" "dse-env.sh" "cassandra.yaml" "dse.yaml" "10-write-prom.conf" "jvm.options"}
+      (into #{} (.list (.toFile @helper/temp-dir))))))))
